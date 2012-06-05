e58b9cb2919c8a8d19b9255c5ab726528cfd361d
Robert BuRnEr Schadek <rburners@gmail.com>
2012-06-05 20:51:30 +0200

    muha much nixer

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,20 +10,22 @@
 #include <loggingmainwidget.hpp>
 #include <debug.hpp>
 
+#include <QIcon>
+
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
 	ui.setupUi(this);
 	QObject::connect(ui.actionGetAll, SIGNAL(triggered()),
 		this, SLOT(getAll()));
 
 	// add tab widgets
-	ui.tabWidget->addTab(new SummaryMainWidget(), tr("&Summary"));
-	ui.tabWidget->addTab(new MailMainWidget(), tr("&Mail"));
-	ui.tabWidget->addTab(new CalendarMainWidget(), tr("&Calendar"));
-	ui.tabWidget->addTab(new ContactsMainWidget(), tr("C&ontacts"));
-	ui.tabWidget->addTab(new InstantMainWidget(), tr("&Instant"));
-	ui.tabWidget->addTab(new IrcMainWidget(), tr("I&rc"));
-	ui.tabWidget->addTab(new TodoMainWidget(), tr("&Todo"));
-	ui.tabWidget->addTab(new LoggingMainWidget(), tr("&Logggin"));
+	ui.tabWidget->addTab(new SummaryMainWidget(), QIcon(":/icons/lazy"), tr("&Summary"));
+	ui.tabWidget->addTab(new MailMainWidget(), QIcon(":/icons/letter"), tr("&Mail"));
+	ui.tabWidget->addTab(new CalendarMainWidget(), QIcon(":/icons/book"), tr("&Calendar"));
+	ui.tabWidget->addTab(new ContactsMainWidget(), QIcon(":/icons/people"), tr("C&ontacts"));
+	ui.tabWidget->addTab(new InstantMainWidget(), QIcon(":/icons/hands"), tr("&Instant"));
+	ui.tabWidget->addTab(new IrcMainWidget(), QIcon(":/icons/trash"), tr("I&rc"));
+	ui.tabWidget->addTab(new TodoMainWidget(), QIcon(":/icons/wrench"), tr("&Todo"));
+	ui.tabWidget->addTab(new LoggingMainWidget(), QIcon(":/icons/dog"), tr("&Logggin"));
 }
 
 void MainWindow::getAll() {
