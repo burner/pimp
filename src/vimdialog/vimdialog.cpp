54c82a091a90d966a11a60ab2efbd2face8f3d40
Robert BuRnEr Schadek <rburners@gmail.com>
2013-04-26 17:14:25 +0200

    save

--- src/vimdialog/vimdialog.cpp
+++ src/vimdialog/vimdialog.cpp
@@ -1,10 +1,20 @@
 #include <QProcess>
 #include <vimdialog/vimdialog.hpp>
+#include <vimdialog/uniquename.hpp>
 
 VimDialog::VimDialog(QTextEdit* te) : textEdit(te), lineEdit(nullptr) {}
 VimDialog::VimDialog(QLineEdit* le) : textEdit(nullptr), lineEdit(le) {}
 
 void VimDialog::run() {
 	QProcess vim;
+	QStringList args;
+	args.push_back("-e");
+	args.push_back("vim");
+	//QString fn = QString::fromStdString(UniqueName::getName());
+	args.push_back(QString::fromStdString(UniqueName::getName()));
+	QString termStr = "roxtern";
+	vim.start(termStr, args);
+	vim.waitForFinish(1000*60*60);
+	vim.kill();
 
 }
